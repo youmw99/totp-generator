2f26598952f2bbb16c83c2cab09223febe6f2c47
YMW <youmw99@gmail.com>
2022-03-01 00:26:02 +0900

    initialization commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const totp = require("totp-generator");
+  const token = totp(req.params.key);
+  res.json('index', { token: token });
+});
+
+module.exports = router;
