a35d05fc7932241664fc9fccdc11464e5423f77a
YMW <youmw99@gmail.com>
2022-03-01 00:32:39 +0900

    error fixed

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const totp = require("totp-generator");
-  const token = totp(req.params.key);
+  const token = totp(req.query.key);
   res.json('index', { token: token });
 });
 
